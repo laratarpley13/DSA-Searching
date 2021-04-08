edc4e49df5a40369174d5d26f3a99b6f3c73f611
laratarpley13 <ltarp13@gmail.com>
2021-04-08 14:11:30 -0500

    drills

---
+++ drills.js
@@ -0,0 +1,22 @@
+/*
+How many searches?
+    (a) 11, 8 (found)
+    (b) 11, 15, 17, error
+*/
+
+//Adding a React UI
+
+//Find a book
+
+/*
+Searching in a BST:
+*/
+
+/*
+Find the next commanding officer:
+    Captain Picard, Commander Riker, Commander Data, 
+    Lt. Cmdr. Worf, Lt. Cmdr. LaForge, Lt. Cmdr. Crusher, 
+    Lt. Security-officer, Lt. Selar.
+*/
+
+//Max profit
